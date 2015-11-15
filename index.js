05b7a5286d1c5b0076c32a4952a4cd67f784d433
Ampakinetic <glasermeister@gmail.com>
2015-11-15 14:04:53 +1300

    Added shutdown support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var service = require('os-service');
+var fs = require('fs');
 
 if (process.argv[2] == "--add") {
     service.add ("Node.JS Process Manager (PM2) Service", {programArgs: ["--run"]}, function(error){ 
@@ -11,27 +12,41 @@ if (process.argv[2] == "--add") {
     service.remove ("Node.JS Process Manager (PM2) Service", function(error){ 
        if (error)
           console.trace(error);
-    }););
+    });
+    
 } else if (process.argv[2] == "--run") {
     var logStream = fs.createWriteStream (process.argv[1] + ".log");
 
     service.run (logStream, function () {
-        service.stop (0);
+    	  var spawnkill = require('child_process').spawn,
+				lskill    = spawnkill('cmd.exe', ['/c', 'pm2 kill']);
+				lskill.stdout.on('data', function (data) {
+					console.log('shutdown stdout: ' + data);
+				});
+				
+				lskill.stderr.on('data', function (data) {
+						console.log('shutdown stderr: ' + data);
+				});
+				
+				lskill.on('exit', function (code) {
+						console.log('shutdown, process exited with code ' + code);
+						service.stop (0);
+				});
     });
 
     var spawn = require('child_process').spawn,
 		ls    = spawn('cmd.exe', ['/c', 'pm2 resurrect']);
 		
 		ls.stdout.on('data', function (data) {
-		console.log('stdout: ' + data);
+			console.log('stdout: ' + data);
 		});
 		
 		ls.stderr.on('data', function (data) {
-		console.log('stderr: ' + data);
+				console.log('stderr: ' + data);
 		});
 		
 		ls.on('exit', function (code) {
-		console.log('child process exited with code ' + code);
+			console.log('child process exited with code ' + code);
 		});
 } else {
     // Show usage...
